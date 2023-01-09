cde7b661dbff942ebcc08288269861c949ba26c7
1murda <gonzalo[1;7rfi.uba.ar>
2023-01-09 00:41:37 -0300

    feat: Add serializers for Cart and CartItem models

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, CartItem, Cart
 
 class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'category', 'description', 'price', 'image', 'created_at')
         read_only_fields = ('id','created_at')
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ('id', 'user', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ('id', 'cart', 'product')
+
 

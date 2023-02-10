8f4ff685e28092df1f0825de1563fefff3029557
6onza <gvineg@fi.uba.ar>
2023-02-10 05:49:21 -0300

    Feat: added views to add cart items and make payment

--- products/views.py
+++ products/views.py
@@ -1,47 +1,109 @@
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated,IsAdminUser 
-from rest_framework.authentication import BasicAuthentication
-from .models import Product, CartItem, Cart
-from .serializers import ProductsSerializer
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.db import IntegrityError
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authentication import TokenAuthentication
-       
+from django.contrib.auth import authenticate
+import stripe
+import environ
+
+from .models import Cart, CartItem, Order, OrderItem, Product
+from .serializers import CartItemSerializer, OrderSerializer, ProductsSerializer
+
+env = environ.Env()
 
 class ProductsView(viewsets.ModelViewSet):
-    
-    serializer_class = ProductsSerializer
     queryset = Product.objects.all()
-    
-    def get(self, request, pk):
-        if pk:
-            product = self.queryset.get(pk=pk)
-            serializer = self.serializer_class(product)
+    serializer_class = ProductsSerializer
 
-            return Response(serializer.data)
-        else:
-            products = self.queryset.all()
-            serializer = self.serializer_class(products, many=True)
+
+stripe.api_key = env('STRIPE_SECRET_KEY')
+
+class CartItemCreateView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        user = request.user
+        cart = Cart.objects.filter(user=user, checked_out=False).first()
+
+        if not cart:
+            cart = Cart.objects.create(user=user)
+        try:
+            product = Product.objects.get(id=request.data.get("product_id"))
+            cart_item = CartItem.objects.create(cart=cart, product=product)
+
+        except Product.DoesNotExist:
+            return Response({"error": "Product does not exist."}, status=status.HTTP_400_BAD_REQUEST)
             
-            return Response(serializer.data)
+        except IntegrityError:
+            return Response({"error": "This item is already in your cart."}, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk):
-        product = self.queryset.get(pk=pk)
-        serializer = ProductsSerializer(product, data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            
-            return Response(serializer.data)
-        
-        return Response(serializer.errors)
-    
-    def delete(self, request, pk):
-        product = self.queryset.get(pk=pk)
-        product.delete()
-        
-        return Response(status=204)
+        serializer = CartItemSerializer(cart_item)
 
-    
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CartItemDeleteView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        user = request.user
+        cart = Cart.objects.filter(user=user, checked_out=False).first()
+
+        if not cart:
+            return Response({"error": "There is no cart for this user."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            product = Product.objects.get(id=request.data.get("product_id"))
+            cart_item = CartItem.objects.filter(cart=cart, product=product).first()
+            cart_item.delete()
+
+        except Product.DoesNotExist:
+            return Response({"error": "Product does not exist."}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class OrderCreateView(APIView):
+    def post(self, request):
+        user = request.user
+        cart = Cart.objects.filter(user=user, checked_out=False).first()
+
+        if not cart:
+            return Response({"error": "There is no cart for this user."}, status=status.HTTP_400_BAD_REQUEST)
+
+        cart_items = CartItem.objects.filter(cart=cart)
+
+        if not cart_items:
+            return Response({"error": "The cart is empty."}, status=status.HTTP_400_BAD_REQUEST)
+
+        total_price = 0
+
+        for item in cart_items:
+            total_price += item.product.price
+
+        try:
+            charge = stripe.Charge.create(
+                amount=int(total_price * 100),
+                currency="usd",
+                source=request.data.get("stripeToken"),
+                description="Charge for " + user.username + " - " + user.email
+            )
+        except stripe.error.CardError as e:
+            return Response({"error": "Your card was declined."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        order = Order.objects.create(user=user)
+
+        for item in cart_items:
+            OrderItem.objects.create(order=order, product=item.product)
+
+        cart.checked_out = True
+        cart.save()
+        serializer = OrderSerializer(order)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

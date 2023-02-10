ab99139f2f48a8953b08a1789d3d1a4cba34fe3b
6onza <gvineg@fi.uba.ar>
2023-02-10 05:48:18 -0300

    Feat: added paths to add and delete cart items

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from products.views import ProductsView
+from products.views import ProductsView, OrderCreateView, CartItemCreateView, CartItemDeleteView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -10,4 +10,7 @@ router.register(r'products', ProductsView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('order/', OrderCreateView.as_view()),
+    path('cart-add-item/', CartItemCreateView.as_view()),
+    path('cart-delete-item/', CartItemDeleteView.as_view()),
 ]
